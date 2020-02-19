ac22a43f2ee695e46859554f79a13be62f12c7a7
Alberto Ramponi <alberto.ramponi@gmail.com>
2020-02-19 10:46:04 +0100

    nuove query

--- file_SQL/query.sql
+++ file_SQL/query.sql
@@ -71,7 +71,7 @@ select * from prenotazioni where data_inizio like "2020-02-20%";
 
 select marca,modello,targa,data_fine as disponibile_da
 from auto join prenotazioni on auto.id = prenotazioni.id_auto
-where data_fine > curdate() and id_auto = 1;
+where data_fine > now() and id_auto = 2;
 /*quando sarà disponibile un'auto che ora non lo è?*/
 
 select marca,modello,targa,sum(spesa) as spesa_totale
@@ -94,7 +94,7 @@ from auto join patenti on auto.id_patente = patenti.id
 join possesso_patenti on patenti.id = possesso_patenti.id_patente
 join dipendenti on dipendenti.id = possesso_patenti.id_dipendente
 where auto.id_patente = possesso_patenti.id_patente
-and dipendenti.id=1;
+and dipendenti.id=3;
 /*auto che posso guidare con la mia patente se non sono neoP*/
 
 select marca,modello,targa,regione,citta
@@ -115,6 +115,48 @@ from prenotazioni as p1 join prenotazioni as p2 on p1.id_auto = p2.id_auto
 join auto on auto.id = p1.id_auto join dipendenti on dipendenti.id = p1.id_dipendente
 where p2.data_fine <= "2019-07-31" 
 and p1.data_fine >= "2019-07-01" 
+and p1.km is not null
 order by p2.data_fine desc, p1.data_fine asc limit 1;
 /*km percorsi presi dalle prenotazioni*/
+
+select marca,modello,targa,(p2.km-p1.km) as km_percorsi
+from prenotazioni as p1 join prenotazioni as p2 on p1.id_auto = p2.id_auto
+join auto on auto.id = p1.id_auto join dipendenti on dipendenti.id = p1.id_dipendente
+where p2.data_fine <= "2019-07-31" 
+and p1.data_fine >= "2019-07-01" 
+and p1.km is not null
+and p1.id_auto = 6
+order by p2.data_fine desc, p1.data_fine asc limit 1;
+/*km fatto da un MEZZO in un determinato perdiodo*/
+
+select nome,cognome,tipologia,data_possesso,year(curdate()) - year(data_possesso)
+-(date_format(curdate(), '%m%d')< date_format(data_possesso, '%m%d')) as durata_possesso
+from dipendenti join possesso_patenti on dipendenti.id = possesso_patenti.id_dipendente
+join patenti on patenti.id = possesso_patenti.id_patente
+where id_dipendente = 3;
+/*da quanto tempo ha la patente un dipendente, essenziale per neoP*/
+
+select dipendenti.id as id_dip,nome,cognome,sede.id as id_sede,livelli.id as id_livello
+from dipendenti join livelli on dipendenti.id_livello = livelli.id
+join sede_dip on dipendenti.id = sede_dip.id_dipendente
+join sede on sede.id = sede_dip.id_sede
+where user_name = "arjmor95";
+/*dati fondamentali per il costruttore del dipendente*/
+
+select marca,modello,targa,data_inizio,data_fine,km
+from dipendenti join prenotazioni on dipendenti.id = prenotazioni.id_dipendente
+join auto on prenotazioni.id_auto = auto.id
+where km is null and id_dipendente = 1
+order by data_fine limit 1;
+/*ultima prenotazione, per aggiunta km*/
+
+select marca,modello,targa,causale.descrizione,data_inizio,data_fine,spesa
+from auto left join prenotazioni on auto.id = prenotazioni.id_auto
+left join causale on prenotazioni.id_causale = causale.id
+left join spesa_manutenzione on auto.id = spesa_manutenzione.id_auto
+where (id_causale = 1 and data_fine is null or id_causale = 2 and data_fine is null);
+/*inserimento dati di fine manutenzione (BOTTONE PER RECUPERARE DATA FINE)*/
+
+
+
 
